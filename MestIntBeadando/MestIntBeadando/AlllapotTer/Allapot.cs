14456d5a2b807cde7454712586cff7513b56878b
Donkó Márk <donkomark@gmail.com>
2020-05-01 18:22:36 +0200

    Operátor meg írása.

---
+++ MestIntBeadando/MestIntBeadando/AlllapotTer/Allapot.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MestIntBeadando.AlllapotTer
+{
+    class Allapot
+    {
+        public static int fekete = 3;
+        public static int feher = 3;
+        private string[,] poziciok = new string[3, 3];
+
+
+        public string[,] Poziciok
+        {
+            get
+            {
+                return poziciok;
+            }
+            set
+            {
+                poziciok = value;
+            }
+        }
+        public Allapot()
+        {
+            for (int i = 0; i < feher; i++)
+            {
+                poziciok[3, i] = "FehL";
+            }
+            for (int i = 0; i < feher; i++)
+            {
+                poziciok[2, i] = "0";
+            }
+            for (int i = 0; i < fekete; i++)
+            {
+                poziciok[1, i] = "FekL";
+            }
+        }
+        public bool celAlllapot()
+        {
+            for (int i = 0; i < feher; i++)
+            {
+                if(poziciok[3, i] == "FehL")
+                {
+                    return false;
+                }
+            }
+            for (int i = 0; i < fekete; i++)
+            {
+                if (poziciok[1, i] == "FekL")
+                {
+                    return false;
+                }
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                if (poziciok[2, i] == "0")
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        public override bool Equals(object obj)
+        {
+            Allapot masikallapot = (Allapot)obj;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (poziciok[j, i] != masikallapot.Poziciok[j, i])
+                    {
+                        return false;
+                    }
+                }
+               
+            }
+            return true;
+        }
+    }
+}
--- MestIntBeadando/MestIntBeadando/AlllapotTer/Csomopont.cs
+++ MestIntBeadando/MestIntBeadando/AlllapotTer/Csomopont.cs
@@ -8,5 +8,82 @@ namespace MestIntBeadando.AlllapotTer
 {
     class Csomopont
     {
+        private Allapot allapot;
+        public Allapot Allapot
+        {
+            get { return allapot; }
+            set { allapot = value; }
+        }
+        private int index;
+        public int Index
+        {
+            get { return index; }
+            set { index = value; }
+        }
+
+        private Csomopont szulo;
+
+        public Csomopont Szulo
+        {
+            get { return szulo; }
+            set { szulo = value; }
+        }
+
+        private int koltseg;
+
+        public int Koltseg { get => koltseg; set => koltseg = value; }
+        public int Heurisztika { get => heurisztika; set => heurisztika = value; }
+        public int OsszKoltseg { get => osszKoltseg; set => osszKoltseg = value; }
+
+        public Csomopont(Allapot allap, int index)
+        {
+            this.index = index;
+            this.allapot = allap;
+        }
+
+        private int heurisztika;
+
+        private int osszKoltseg;
+
+        public Csomopont(Allapot allap, int index, Csomopont szulo)
+        {
+            this.index = index;
+            this.allapot = allap;
+            this.szulo = szulo;
+            if (szulo == null)
+            {
+                Koltseg = 1;
+            }
+            else
+            {
+                Koltseg = szulo.Koltseg + 1;
+            }
+            this.heurisztika = heurisztikaSzamolas();
+
+            this.osszKoltseg = Koltseg + Heurisztika;
+        }
+        private int heurisztikaSzamolas()
+        {
+            //P=3, Q=2, R=1
+            int suly = 1;
+
+            foreach (String pozicio in allapot.Poziciok)
+            {
+                if (pozicio==pozicio)
+                {
+                    suly += 3;
+                }
+                else if (korong == "Q")
+                {
+                    suly += 2;
+                }
+                else
+                {
+                    suly += 1;
+                }
+            }
+            return suly;
+        }
     }
+
 }
